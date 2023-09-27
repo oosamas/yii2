e551358c5c9a5d30ac2b24ce36caded882c61fba
jawwadeuro <114021997+jawwadeuro@users.noreply.github.com>
2023-09-27 17:10:44 +0500

    Once again

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -34,7 +34,7 @@ $this->title = Yii::$app->name;
             <div class="col-lg-4">
                 <h2>Here we go again</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                <p>Once again. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                     fugiat nulla pariatur.</p>
