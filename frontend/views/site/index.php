0793fdcea6778e8ddea9e5843fb2308491591fc1
OSB <osaddique@gmail.com>
2023-12-04 15:04:27 -0500

    frontend

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,6 +2,8 @@
 /**
  * @var yii\web\View $this
  */
+
+
 $this->title = Yii::$app->name;
 ?>
 <div class="site-index">
@@ -33,10 +35,7 @@ $this->title = Yii::$app->name;
             <div class="col-lg-4">
                 <h2>Heading</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>WTF is working</p>
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>
